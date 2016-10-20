b591c58777638f193e756c9d541bb615d2ed68d0
Baggers <chris.bagley@gmail.com>
2016-10-20 18:28:31 +0200

    sphere higher poly by default

--- dendrite.primitives/primitives.lisp
+++ dendrite.primitives/primitives.lisp
@@ -281,8 +281,8 @@
           (list 0 1 2 0 2 3 4 5 6 4 6 7 8 9 10 8 10 11 12 13 14 12 14 15 16 17
                 18 16 18 19 20 21 22 20 22 23))))
 
-(defun sphere-data (&key (radius 0.5) (lines-of-latitude 10)
-                      (lines-of-longitude 10) (normals t) (tex-coords t))
+(defun sphere-data (&key (radius 0.5) (lines-of-latitude 30)
+                      (lines-of-longitude 30) (normals t) (tex-coords t))
   (declare (type (unsigned-byte 8) lines-of-longitude lines-of-latitude))
   ;; latitude  -  horizontal
   ;; longitude -  vertical
