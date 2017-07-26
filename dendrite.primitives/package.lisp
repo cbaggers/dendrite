455766a59044aa058730fc41c18c884ae7bd370f
cbaggers <chris.bagley@gmail.com>
2017-07-26 18:00:00 +0200

    fuck knows how this happened

--- dendrite.primitives/package.lisp
+++ dendrite.primitives/package.lisp
@@ -2,7 +2,7 @@
 
 (uiop:define-package :dendrite.primitives
   (:use :cl :rtg-math :rtg-math.base-maths :cffi)
-  (:export :lattice-data
+  (:export :latice-data
            :primitive-data
            :cap-data
            :plain-data
@@ -15,7 +15,7 @@
            :swap-winding-order
 
            ;; Versions returning cffi pointers
-           :lattice-foreign
+           :latice-foreign
            :plain-foreign
            :box-foreign
            :cube-foreign
